8ef059c34b90649c4d4b1303ce99d71bbc14f5b7
gustavo.almeida <ghalmeida18@gmail.com>
2023-02-17 09:39:39 -0300

    Added first get.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+
+driver = Chrome()
+
+driver.get('https://www.magazineluiza.com.br/busca/celulares/?filters=brand---apple')
+
+value1 = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/section[4]/div[3]/div/ul/li[1]/a/div[3]/div[2]/div/p[2]')
+
+print(value1.text)
