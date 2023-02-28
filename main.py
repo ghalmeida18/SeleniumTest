fb10672cea621962b2277ffeaeb63adf1b5c020e
gustavo.almeida <ghalmeida18@gmail.com>
2023-02-28 09:34:37 -0300

    Site changed

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from selenium.webdriver.common.by import By
 
 driver = Chrome()
 
-driver.get('https://www.magazineluiza.com.br/busca/celulares/?filters=brand---apple')
+driver.get('https://www.kohls.com/catalog/mens-nike-shoes.jsp?CN=Gender:Mens+Brand:Nike+Department:Shoes&icid=sl-nav-ftw-shoes-mens-nike&kls_sbp=84311432945461347682116220168293182461')
 
-value1 = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/section[4]/div[3]/div/ul/li[1]/a/div[3]/div[2]/div/p[2]')
+value1 = driver.find_element(By.XPATH, '//*[@id="902047_prod_price"]/div[1]/span[1]')
 
-print(value1.text)
+value2 = driver.find_element(By.XPATH, '//*[@id="5013248_prod_price"]/div[1]/span[1]')
+
+print(f'The product 1 has value: {value1.text}. The product 2 has value: {value2.text}')
+
